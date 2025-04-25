4c4894ad4975e829c0b2f0bd418ebd09a194dd95
Rene Marroquin <rene.marroquin131420@outlook.com>
2025-04-24 21:52:37 -0600

    Removed all files from GitHub

--- python_projects/weather_projects/json_test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-json_path = r"C:\Users\renem\Documentos\Python with AI\python_projects\python_projects\weather_projects\ivr_simulator_RT.json"
-
-print("Checking path:", json_path)
-if os.path.exists(json_path):
-    print("✅ JSON file found at:", json_path)
-else:
-    print("❌ JSON file not found!")
